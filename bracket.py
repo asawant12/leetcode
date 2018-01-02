5a0df65b567937e786389b08f2338b227638c41c
amit sawant <sawantamitm@gmail.com>
2018-01-02 14:14:45 -0800

    Create bracket.py

---
+++ bracket.py
@@ -0,0 +1,13 @@
+def print_brackets(number):
+  if not number:
+    return ''
+  return _print_brackets('',number,0,0)
+
+def _print_brackets(inp,number,open,close):
+  if close == number:
+    print(inp)
+    return
+  if open > close:
+    _print_brackets(inp+'}',number,open,close+1)
+   if open < number:
+    _print_brackets(inp+'{',number,open+1,close)
