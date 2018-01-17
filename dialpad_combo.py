4e39096341b2837e8b4e32255befce22d3ae0890
amit sawant <sawantamitm@gmail.com>
2018-01-17 10:43:22 -0800

    Create dialpad_combo.py

---
+++ dialpad_combo.py
@@ -0,0 +1,18 @@
+def dialpad(digits):
+  if not len(digits):
+    return []
+  mapping= { '2' : 'ABC',
+             '3' : 'DEF',
+             '4' : 'GHI',
+             '5' : 'JKL',
+             '6' : 'MNO',
+             '7' : 'PQRS',
+             '8' : 'TUV',
+             '9' : 'WXYZ'}
+             
+if len(digits) == 1:
+  return mapping[digits[0]]
+
+prev = dialpad(digits[:-1])
+additional = mapping[digits[-1]]
+return [ s + c for s in prev for c in additional]
