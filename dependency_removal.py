ee48c4b3fa4feb2961515a752133fbeea5139aa4
amit sawant <sawantamitm@gmail.com>
2018-01-17 10:34:35 -0800

    Create dependency_removal.py

---
+++ dependency_removal.py
@@ -0,0 +1,29 @@
+'''
+A : B
+A : D
+B : C
+B : E
+C : E
+C : D
+'''
+
+Class Node:
+  def __init(self,name):
+    self.name = name
+    self.edges= []
+  
+  def add_edge(self,node):
+    self.dge.append(node)
+
+resolved = []
+seen = []
+
+def resolve_dep(node,seen,resolved):
+  seen.append(node)
+  for edge in node.edges:
+    if edge not in resolved:
+      if edge in seen:
+        raise "Cycle"
+       resolve_dep(edge,seen,resolved)
+    resolved.append(node)
+  
