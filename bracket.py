1a16bd699ef6bbd3dd14b35d08ffde412497a20f
amit sawant <sawantamitm@gmail.com>
2020-07-17 14:58:14 -0700

    Update bracket.py

--- bracket.py
+++ bracket.py
@@ -1,16 +1,17 @@
 # code to print multiple valid combinations of paranthesis
 
 class Solution(object):
+    def backtrack(S = '', left = 0, right = 0):
+        if len(S) == 2 * N:
+            ans.append(S)
+            return
+        if left < N:
+           self.backtrack(S+'(', left+1, right)
+        if right < left:
+           self.backtrack(S+')', left, right+1)
     def generateParenthesis(self, N):
         ans = []
-        def backtrack(S = '', left = 0, right = 0):
-            if len(S) == 2 * N:
-                ans.append(S)
-                return
-            if left < N:
-                backtrack(S+'(', left+1, right)
-            if right < left:
-                backtrack(S+')', left, right+1)
+        self.backtrack()
 
-        backtrack()
+        
         return ans
