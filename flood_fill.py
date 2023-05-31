ac2fe5825fd26b772fd65477115ace91643d875f
amit sawant <sawantamitm@gmail.com>
2023-05-31 19:47:30 +0530

    Create flood_fill.py

---
+++ flood_fill.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/flood-fill/
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        def bfs(image,sr,sc,color, base_color):
+            if sr >= len(image) or sc >= len(image) or sc < 0 or sr < 0:
+                return image
+            if color == image[sr][sc]:
+                return image
+            if image[sr][sc] == base_color:
+                image[sr][sc] = color
+            image = bfs(image,sr,sc+1, color, base_color)
+            image = bfs(image,sr,sc-1, color, base_color)
+            image = bfs(image,sr+1,sc, color, base_color)
+            image = bfs(image,sr-1,sc, color, base_color)
+            return image
+        return bfs(image,sr,sc,color,image[sr][sc])
+                
+                
+        
+        
