19e9a1143fc5c8100bdc3a43480a0ce3a29e042d
amit sawant <sawantamitm@gmail.com>
2023-06-11 16:54:57 -0700

    Update decompress_1.py

--- decompress_1.py
+++ decompress_1.py
@@ -1,1 +1,16 @@
 # https://leetcode.com/problems/decompress-run-length-encoded-list/
+
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        
+        index=0
+        total = len(nums)
+        decomperss = []
+        while index < total-1:
+            freq = nums[index]
+            val = nums[index+1]
+            decomp_list = [val]*freq
+            decomperss.extend(decomp_list)
+            index += 2
+        return decomperss
+            
