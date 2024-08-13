6d148958189046997fe68b62f3c1d10a03293e14
amit sawant <sawantamitm@gmail.com>
2024-08-13 05:31:10 -0700

    Create max_heap.py

---
+++ max_heap.py
@@ -0,0 +1,60 @@
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def _left_child(self, index):
+        return 2 * index + 1
+
+    def _right_child(self, index):
+        return 2 * index + 2
+
+    def _parent(self, index):
+        return (index - 1) // 2
+
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
+        
+    def insert(self, val):
+        self.heap.append(val)
+        current = len(self.heap)-1
+        while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
+            self._swap(self._parent(current), current)
+            current = self._parent(current)
+
+    # WRITE THE INSERT METHOD HERE #
+    #                              #
+    #                              #
+    #                              #
+    #                              #
+    ################################
+    
+    
+    
+myheap = MaxHeap()
+myheap.insert(99)
+myheap.insert(72)
+myheap.insert(61)
+myheap.insert(58)
+
+print(myheap.heap)  
+
+
+myheap.insert(100)
+
+print(myheap.heap)  
+
+
+myheap.insert(75)
+
+print(myheap.heap)
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [99, 72, 61, 58]
+    [100, 99, 61, 58, 72]
+    [100, 99, 75, 58, 72, 61]
+
+"""
+    
