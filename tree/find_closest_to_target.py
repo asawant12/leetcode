c365181240ca8c59f4e59ded88dc4d0c0bfb1d9b
amit sawant <sawantamitm@gmail.com>
2024-10-09 18:07:23 -0700

    Update find_closest_to_target.py

--- tree/find_closest_to_target.py
+++ tree/find_closest_to_target.py
@@ -4,14 +4,15 @@ closest = float('inf')
 def find_closest(root, target):
   if root is None:
     return
-  if root.val == target:
-    return root.val
   if abs(root.val - target) < abs(closest - target):
     closest = root.val
   if target < root.val:
     find_closest(root.left, target)
-  else:
+  elif target > root.val::
     find_closest(root.right, target)
+  else:
+    closest = root.val
+    return
 
 
   find_closest(root, target)
